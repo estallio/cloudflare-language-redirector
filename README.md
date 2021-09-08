c7bc5720ce463610579e3941c785f41ceed23c08
Leonhard Esterbauer <leonhard.esterbauer@gmail.com>
2021-09-08 21:34:38 +0200

    polished code style

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Redirecting Languages
 
 Redirects all paths to `de` and `en` languages expect:
+
 ```
 'sw.js',
 'sitemap.xml',
@@ -17,6 +18,7 @@ Redirects all paths to `de` and `en` languages expect:
 This is necessary as all incoming request have to go through the worker because there are no look ahead patterns like "execute this worker when there is no '/de' in the path". Also common plugins like the [next-i18next](https://github.com/isaachinman/next-i18next) do it like this which can be seen in their [code on Github](https://github.com/isaachinman/next-i18next/blob/abdf06545410f340b0529e3448f8b102ab840249/src/config/default-config.ts#L27). The second `/` in de and en paths is necessary as the code is transformed into a Regex in JS where slashes and backslashes have to be escaped.
 
 #### Language recognition order:
+
 1. check if path is not a static or excluded one
 2. check if language was specified in the language cookie
 3. check if a supported language is given in the browser languages
--- index.js
+++ index.js
@@ -12,97 +12,102 @@ Language recognition order:
 
 // According to https://worldpopulationreview.com/country-rankings/german-speaking-countries
 // german speaking countries are
-const germanSpeakingCountries = [
-  'DE',
-  'AT',
-  'CH',
-  'BE',
-  'LU',
-  'LI'
-]
+const germanSpeakingCountries = ['DE', 'AT', 'CH', 'BE', 'LU', 'LI']
 
 const langCookieName = 'i18n_redirected'
 
-const supportedLanguages = [
-  'de',
-  'en'
-]
+const supportedLanguages = ['de', 'en']
 
 const defaultLanguage = 'en'
 
 const staticPaths = [
-  'sw.js',
-  'sitemap.xml',
-  'robots.txt',
-  'icon.png',
-  'favicon.ico',
-  '_nuxt\\/.*',
-  'static\\/.*',
-  'de(\\/.*)?',
-  'en(\\/.*)?',
-  '404.html'
+    'sw.js',
+    'sitemap.xml',
+    'robots.txt',
+    'icon.png',
+    'favicon.ico',
+    '_nuxt\\/.*',
+    'static\\/.*',
+    'de(\\/.*)?',
+    'en(\\/.*)?',
+    '404.html',
 ]
 
 const staticPathsRegex = new RegExp(`^/(${staticPaths.join('|')})$`, 'i')
 
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
-  const url = new URL(request.url)
+    const url = new URL(request.url)
 
-  ////////
-  // case 1: check if path is not a static or excluded one
-  ////////
+    ////////
+    // case 1: check if path is not a static or excluded one
+    ////////
 
-  if (url.pathname.match(staticPathsRegex)) {
-    return fetch(request)
-  }
-
-  ////////
-  // case 2: cookie is set
-  ////////
-
-  // extract cookies of cookie http header
-  const cookies = cookieParser(request.headers.get('Cookie') || '')
-
-  // check if cookie (key in the cookies object) exists
-  if (cookies[langCookieName] !== null) {
-    // check if language is supported
-    if(supportedLanguages.includes(cookies[langCookieName])) {
-      // respond with the cookie value
-      return Response.redirect((url.origin + '/' + cookies[langCookieName] + url.pathname).replace(/\/+$/, ''), 302)
+    if (url.pathname.match(staticPathsRegex)) {
+        return fetch(request)
     }
-  }
 
-  ////////
-  // case 3: check if a supported language is given in the browser languages
-  ////////
+    ////////
+    // case 2: cookie is set
+    ////////
 
-  // get the favorite supported language from header - could return null
-  const lang = acceptLanguageParser.pick(
-    supportedLanguages,
-    request.headers.get('Accept-Language') || '',
-    { loose: true }
+    // extract cookies of cookie http header
+    const cookies = cookieParser(request.headers.get('Cookie') || '')
+
+    // check if cookie (key in the cookies object) exists
+    if (cookies[langCookieName] !== null) {
+        // check if language is supported
+        if (supportedLanguages.includes(cookies[langCookieName])) {
+            // respond with the cookie value
+            return Response.redirect(
+                (
+                    url.origin +
+                    '/' +
+                    cookies[langCookieName] +
+                    url.pathname
+                ).replace(/\/+$/, ''),
+                302
+            )
+        }
+    }
+
+    ////////
+    // case 3: check if a supported language is given in the browser languages
+    ////////
+
+    // get the favorite supported language from header - could return null
+    const lang = acceptLanguageParser.pick(
+        supportedLanguages,
+        request.headers.get('Accept-Language') || '',
+        { loose: true }
     )
 
-  if (lang !== null) {
-    // respond with the accept-language value
-    return Response.redirect((url.origin + '/' + lang + url.pathname).replace(/\/+$/, ''), 302)
-  }
+    if (lang !== null) {
+        // respond with the accept-language value
+        return Response.redirect(
+            (url.origin + '/' + lang + url.pathname).replace(/\/+$/, ''),
+            302
+        )
+    }
 
-  ////////
-  // case 4: check if request comes from a german speaking country
-  ////////
-  if (germanSpeakingCountries.includes(request.cf.country.toUpperCase())) {
-    return Response.redirect((url.origin + '/de' + url.pathname).replace(/\/+$/, ''), 302)
-  }
+    ////////
+    // case 4: check if request comes from a german speaking country
+    ////////
+    if (germanSpeakingCountries.includes(request.cf.country.toUpperCase())) {
+        return Response.redirect(
+            (url.origin + '/de' + url.pathname).replace(/\/+$/, ''),
+            302
+        )
+    }
 
-  ////////
-  // case 5: fallback to english as english is understood all over the globe
-  ////////
-  return Response.redirect((url.origin + '/en' + url.pathname).replace(/\/+$/, ''), 302)
+    ////////
+    // case 5: fallback to english as english is understood all over the globe
+    ////////
+    return Response.redirect(
+        (url.origin + '/en' + url.pathname).replace(/\/+$/, ''),
+        302
+    )
 }
-
-
